0dca1f8bea5e9d48707abf5cda514bd6273d0d7f
Nakildias <130524745+Nakildias@users.noreply.github.com>
2025-08-24 20:07:18 -0400

    Add files via upload

--- dungeon-crawler-rg-od.app/assets/js/music.js
+++ dungeon-crawler-rg-od.app/assets/js/music.js
@@ -37,102 +37,102 @@ let sfxBuff;
 const setVolume = () => {
     // ===== BGM =====
     bgmDungeon = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/dungeon.webm', '/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/dungeon.mp3'],
+        src: ['assets/bgm/dungeon.webm', 'assets/bgm/dungeon.mp3'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleMain = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_main.webm', '/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_main.mp3'],
+        src: ['assets/bgm/battle_main.webm', 'assets/bgm/battle_main.mp3'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleBoss = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_boss.webm', '/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_boss.mp3'],
+        src: ['assets/bgm/battle_boss.webm', 'assets/bgm/battle_boss.mp3'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     bgmBattleGuardian = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_guardian.webm', '/static/third-party/apps/dungeon-crawler-rg-od.app/assets/bgm/battle_guardian.mp3'],
+        src: ['assets/bgm/battle_guardian.webm', 'assets/bgm/battle_guardian.mp3'],
         volume: volume.bgm * volume.master,
         loop: true
     });
 
     // ===== SFX =====
     sfxEncounter = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/encounter.wav'],
+        src: ['assets/sfx/encounter.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxCombatEnd = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/combat_end.wav'],
+        src: ['assets/sfx/combat_end.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxAttack = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/attack.wav'],
+        src: ['assets/sfx/attack.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxLvlUp = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/level_up.wav'],
+        src: ['assets/sfx/level_up.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxConfirm = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/confirm.wav'],
+        src: ['assets/sfx/confirm.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxDecline = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/decline.wav'],
+        src: ['assets/sfx/decline.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxDeny = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/denied.wav'],
+        src: ['assets/sfx/denied.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxEquip = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/equip.wav'],
+        src: ['assets/sfx/equip.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxUnequip = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/unequip.wav'],
+        src: ['assets/sfx/unequip.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxOpen = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/hover.wav'],
+        src: ['assets/sfx/hover.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxPause = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/pause.wav'],
+        src: ['assets/sfx/pause.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxUnpause = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/unpause.wav'],
+        src: ['assets/sfx/unpause.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxSell = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/sell.wav'],
+        src: ['assets/sfx/sell.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxItem = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/item_use.wav'],
+        src: ['assets/sfx/item_use.wav'],
         volume: volume.sfx * volume.master
     });
 
     sfxBuff = new Howl({
-        src: ['/static/third-party/apps/dungeon-crawler-rg-od.app/assets/sfx/buff.wav'],
+        src: ['assets/sfx/buff.wav'],
         volume: volume.sfx * volume.master
     });
 }
